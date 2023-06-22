ad42be862d6fdf558e6493eca2b737e56607aa2e
Polojames552 <Polojames552@gmail.com>
2023-06-22 15:22:57 +0800

    pushing to other repo

--- resources/views/Sample.blade.php
+++ resources/views/Sample.blade.php
@@ -60,11 +60,8 @@
         <h1>
             Hello
         </h1>
-        <h1>
-            Hello
-        </h1>
-        <h1>
-            Hello
-        </h1>
+      <h2>
+        Hello
+      </h2>
     </div>
 </html>
